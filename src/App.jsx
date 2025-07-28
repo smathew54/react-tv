55801b9ce3493cd218ab89b980788d0110b51e04
smathew54 <88407507+smathew54@users.noreply.github.com>
2025-07-27 21:05:02 -0500

    adding the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-/**
+  /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+
+import React from "react";
+import { tvShows } from "./shows/data";
+
 export default function App() {
   return (
     <>
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,32 @@
 import "./shows.css";
 
+import EpisodeList from "../episodes/EpisodeList"
+import EpisodeDetails from "../episodes/EpisodeDetails";
+
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  if (!show) {
+    return (
+      <section className="show-details">
+        <h2>Show Details</h2>
+        <p>Select a show to learn more.</p>
+      </section>
+    );
+  }
+  return (
+    <div className="show-details">
+      <div className="handle">
+        <h2> selected color is {show}</h2>
+        <main>
+        <EpisodeList 
+        name ={name} 
+        episodes ={show.episodes} 
+        selectedEpisode = {show.selectedEpisode} 
+        setSelectedEpisode={show.setSelectedEpisode}/>
+        <EpisodeDetails 
+        episode={show.episode}/>
+        </main>
+      </div>
+    </div>
+  );
 }
